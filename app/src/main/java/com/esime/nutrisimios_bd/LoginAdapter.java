43ba279c87ee529f2d7f0730ac83bf2ba47bfe21
JRamonSubias <ramonsubiasmendoza@gmail.com>
2020-12-30 13:51:31 -0600

    Initial commit

---
+++ app/src/main/java/com/esime/nutrisimios_bd/LoginAdapter.java
@@ -0,0 +1,41 @@
+package com.esime.nutrisimios_bd;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class LoginAdapter extends FragmentPagerAdapter {
+    private Context context;
+    int totalTabs;
+
+    public LoginAdapter(FragmentManager fm, Context context, int totalTabs){
+        super(fm);
+        this.context = context;
+        this.totalTabs = totalTabs;
+    };
+
+
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                SignIn signIn = new SignIn();
+                return signIn;
+            case 1:
+            SingUp singUp = new SingUp();
+            return singUp;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+}
---
+++ app/src/main/java/com/esime/nutrisimios_bd/MainActivity.java
@@ -0,0 +1,39 @@
+package com.esime.nutrisimios_bd;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getByID();
+        LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), this,tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+    }
+
+    private void getByID() {
+        tabLayout = findViewById(R.id.loginTabLayout);
+        viewPager = findViewById(R.id.loginView);
+    }
+
+
+
+
+}
